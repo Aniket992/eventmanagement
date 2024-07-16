53cccb82a2856aacca09de817a3d1df9f5523ac0
Aniket992 <78945155+Aniket992@users.noreply.github.com>
2024-07-16 10:09:26 +0530

    eventpage update

--- src/Pages/Eventpage/Eventpage.jsx
+++ src/Pages/Eventpage/Eventpage.jsx
@@ -72,48 +72,38 @@ const EventPage = () => {
   return (
     <>
       <div
-        className=" bg-cover bg-center  bg-fixed   text-white"
+        className=" bg-cover bg-center  bg-fixed  bg-black  text-white"
         style={{ backgroundImage: `url(${eventbg})` }}
       >
-        <div className="w-full h-full bg-black bg-opacity-85">
-          <div className=" flex sm:block justify-between items-center ">
-          <Navbar />
-          <div className=" sm:w-full flex sm:justify-end  sm:p-3 ">
+        <div className="w-full h-full  bg-black bg-opacity-95">
+        <Navbar />
+         
+
+          <div className="w-full flex flex-col   h-full border-2 rounded-xl border-blue-600 shadow ">
+          <div className=" w-full flex items-center    ">
+          <div className="flex flex-col  rounded-xl  w-full   justify-center items-center pt-5 px-3">
+             
+
+             <h2 className=" gradient-text  font-avenger   text-center tracking-widest underline  text-6xl pb-5">
+               {event.eventName}
+             </h2>
+           </div>
                 <button
-                  className="  bg-yellow-400 mr-3 pr-3 text-black p-2 font-avenger rounded border-2 border-blue-950"
+                  className="  bg-yellow-400 sm:justify-end mr-3 pr-3 h-fit text-black p-2 font-avenger rounded border-2 border-blue-950"
                   onClick={handleShare}
                 >
                   Share
                 </button>
               </div>
-          </div>
-          <div className="w-full flex justify-center  ">
-          <div className="p-5 flex flex-col justify-center items-center  bg-black bg-opacity-65
-           text-white font-sans tracking-wider  w-full md:w-5/6 border-2 rounded-xl border-blue-600 mb-2 ">
-            <div className="flex flex-col  rounded-xl bg-black  w-fit  justify-center items-center pt-5 px-3">
-             
-
-              <h2 className=" gradient-text  font-avengerd text-blue-500 text-center tracking-widest underline underline-offset-8  text-6xl pb-5">
-                {event.eventName}
-              </h2>
-            </div>
-            <br />
+              <div className=" flex w-full ">
+              <div className="p-5 flex flex-col     
+           text-white font-sans tracking-wider  w-1/2   ">
+            
             <div className="flex flex-col justify-center items-center ">
-            <div className="   flex flex-wrap justify-between   items-center gap-5 ">
-              {/* <div className="  lg:flex-col lg:w-1/3 flex flex-col md:flex-row justify-evenly items-center gap-5 "> */}
-
-                {event.photos.map((photoId) => (
-                  <img
-                    className="w-64 h-64 rounded-xl"
-                    key={photoId}
-                    src={`${URL}/api/file/view/${photoId}`}
-                    alt="Event"
-                  />
-                ))}
-              </div>
+            
               <div className="  flex flex-col p-5 text-center justify-center  items-center    ">
                 <div className="mb-5">
-                  <p className="font-avenger tracking-widest text-blue-600 text-2xl font-bold">
+                  <p className="font-avenger tracking-widest text-green-600 text-2xl font-bold">
                     {" "}
                     Organiser:
                   </p>
@@ -123,7 +113,7 @@ const EventPage = () => {
                   </p>
                 </div>
                 <div className="mb-5">
-                  <p className="font-avenger tracking-widest text-blue-600 text-2xl font-bold">
+                  <p className="font-avenger tracking-widest text-green-600 text-2xl font-bold">
                     Event Type:
                   </p>
                   <p className="  font-bold text-xl  ">
@@ -132,7 +122,7 @@ const EventPage = () => {
                   </p>
                 </div>
                 <div className=" flex-col  ">
-                  <p className="font-avenger tracking-widest text-blue-600 text-2xl font-bold">
+                  <p className="font-avenger tracking-widest text-green-600 text-2xl font-bold">
                     Description:
                   </p>
                   <p className=" text-justify  text-xl  ">
@@ -142,7 +132,7 @@ const EventPage = () => {
                 <div className="w-full flex flex-col gap-7 ">
                   <div className=" w-full  flex justify-around  flex-col"></div>
                   <div className=" flex flex-col justify-center gap-3 items-center w-full ">
-                    <p className="font-avenger tracking-widest text-blue-600 text-2xl font-bold">
+                    <p className="font-avenger tracking-widest text-green-600 text-2xl font-bold">
                       Eligibilities:
                     </p>
                     <ul className="list-outside list-disc   text-xl text-start  w-full ">
@@ -150,7 +140,7 @@ const EventPage = () => {
                         <li key={index}>{rule}</li>
                       ))}
                     </ul>
-                    <p className="font-avenger tracking-widest text-blue-600 text-2xl font-bold">
+                    <p className="font-avenger tracking-widest text-green-600 text-2xl font-bold">
                       Rules:
                     </p>
                     <ul className="list-outside list-disc  text-xl text-start  tracking-wider w-full ">
@@ -159,14 +149,36 @@ const EventPage = () => {
                       ))}
                     </ul>
                   </div>
-                  <div className="flex justify-center">
-                  <button className="bg-blue-500  text-black font-avenger tracking-widest text-2xl font-bold p-3 w-fit rounded-full" 
+                  <div className="flex justify-evenly">
+                  <button className="bg-green-500  text-black font-avenger tracking-widest text-2xl font-bold p-3 w-fit rounded-full" 
                       onClick={() => download(event.ruleBook)}
                       >Download Rule Book</button>
-
+                  <button className="bg-green-500  text-black font-avenger tracking-widest text-2xl font-bold p-3 w-fit rounded-full" 
+                   > Download Brochure</button>
                   </div>
-                  <div>
-                    <p className="font-avenger tracking-widest text-blue-600 text-2xl font-bold">
+                 
+
+                </div>
+              </div>
+             
+            </div>
+          </div>
+          <div className="  w-1/2 flex flex-col justify-between p-5   ">
+          <div className="   flex flex-wrap justify-between   items-center gap-5 ">
+
+                {event.photos.map((photoId) => (
+                  <img
+                    className="w-64 h-64 rounded-xl"
+                    key={photoId}
+                    src={`${URL}/api/file/view/${photoId}`}
+                    alt="Event"
+                  />
+                ))}
+              </div>  
+              <div className=" h-full text-center flex  flex-col justify-between pb-5 w-full ">
+
+              <div>
+                    <p className="font-avenger tracking-widest text-green-600 text-2xl font-bold">
                       Location:
                     </p>
                     <p className="  font-bold text-xl  ">
@@ -177,7 +189,7 @@ const EventPage = () => {
 
                   <div className="flex justify-around md:flex-row flex-col">
                     <div className=" ">
-                      <p className="font-avenger tracking-widest text-blue-600 text-2xl font-bold">
+                      <p className="font-avenger tracking-widest text-green-600 text-2xl font-bold">
                         Start Date:
                       </p>
                       <p className="  font-bold text-xl  ">
@@ -186,7 +198,7 @@ const EventPage = () => {
                       </p>
                     </div>
                     <div>
-                      <p className="font-avenger tracking-widest text-blue-600 text-2xl font-bold">
+                      <p className="font-avenger tracking-widest text-green-600 text-2xl font-bold">
                         End Date:{" "}
                       </p>
                       <p className="  font-bold text-xl  ">
@@ -195,7 +207,7 @@ const EventPage = () => {
                     </div>
                   </div>
                   <div>
-                    <p className="font-avenger tracking-widest text-blue-600 text-2xl font-bold">
+                    <p className="font-avenger tracking-widest text-green-600 text-2xl font-bold">
                       Last Date of Registration:
                     </p>
                     <p className=" font-bold text-xl  ">
@@ -205,7 +217,7 @@ const EventPage = () => {
                     </p>
                   </div>
                   <div>
-                  <p className="font-avenger tracking-widest text-blue-600 text-2xl font-bold">
+                  <p className="font-avenger tracking-widest text-green-600 text-2xl font-bold">
                   Registration Charges:
                   </p>
                   <p className="  font-bold text-xl  ">
@@ -218,7 +230,7 @@ const EventPage = () => {
                   </p>
                   </div>
                   <div className=" w-full font-bold  ">
-            <h4 className="font-avenger tracking-widest text-blue-600 text-2xl font-bold">Contact:</h4>
+            <h4 className="font-avenger tracking-widest text-green-600 text-2xl font-bold">Contact:</h4>
             {event.contact.map((contact, index) => (
               <div key={index} className="  font-bold text-xl ">
                 <p>Name: {contact.name}</p>
@@ -229,18 +241,17 @@ const EventPage = () => {
                   <div className=" w-full flex justify-center mt-5">
 
                   <button
-                    className="   w-1/2   text-4xl text-black font-extrabold tracking-widest bg-blue-400 p-2   font-avenger rounded-full border-2 border-blue-950"
+                    className="   w-1/2  hero  text-4xl text-black font-extrabold tracking-widest bg-blue-400 p-2   font-avenger rounded-full border-2 border-blue-950"
                     onClick={handleApplyNow}
                   >
                     Apply Now
                   </button>
+                  </div> 
                   </div>
-
-                </div>
-              </div>
-             
-            </div>
+      
           </div>
+              </div>
+          
           </div>
           <Footer/>
         </div>
